6402311dadb2de23c3867fe3a48ffeaae7279c8f
Richard Dodd <richard.o.dodd@gmail.com>
2018-09-02 09:30:11 +0100

    Add feature for skeptic ("ci")

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
+#[cfg(feature = "ci")]
 extern crate skeptic;
 
 fn main() {
+    #[cfg(feature = "ci")]
     skeptic::generate_doc_tests(&["README.md"]);
 }
--- tests/skeptic.rs
+++ tests/skeptic.rs
@@ -1,1 +1,3 @@
+
+#[cfg(feature = "ci")]
 include!(concat!(env!("OUT_DIR"), "/skeptic-tests.rs"));
